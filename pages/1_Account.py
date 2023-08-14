90133d2d440eb53f9fdee665eb1f5b0f12c31aee
moomoh <34252092+moomoh@users.noreply.github.com>
2023-08-14 23:34:03 +0300

    Update 1_Account.py

--- pages/1_Account.py
+++ pages/1_Account.py
@@ -55,7 +55,7 @@ else:
     client_secret =""
     redirect_uri ="https://st-mp-go.streamlit.app/Main"
 
-    client = GoogleOAuth2(client_id)
+    client = GoogleOAuth2(client_id,client_secret)
     authorization_url = asyncio.run(
         write_authorization_url(client=client, redirect_uri=redirect_uri)
     )
